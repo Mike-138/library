c998a9171e5ee463ba021b59e1b26dfc52bc7f12
Michael <mikemcd.bbs@gmail.com>
2023-09-22 16:48:30 -0400

    Add book object in js

--- app.js
+++ app.js
@@ -24,3 +24,14 @@ function Carousel() {
         return this.render();
     }
 }
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`
+    }
+}
